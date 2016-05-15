a58da6f40121547c03876c8f3f44d22544dac089
bluhuu <bluhuu@gmail.com>
2016-05-15 11:55:48 +0800

    Update proxy.config.js

--- proxy.config.js
+++ proxy.config.js
@@ -1,3 +1,7 @@
-
-module.exports = {};
-
+module.exports = {
+    //'GET http://127.0.0.1:8989/index.html': 'http://www.qq.com/',
+    //'GET http://192.168.1.104:': 'http://www.lalilali.com/',
+    'http://localhost:8989/try/angularjs/data/*': 'http://www.runoob.com:80',
+    'http://localhost:8989/CloudNote/*':'http://localhost:8088'
+    //'http://www.lalilali.com': 'http://www.qq.com',
+};
