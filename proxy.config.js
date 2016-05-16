99742407e3686c4160c58af98ac10bc303bd431c
bluhuu <bluhuu@gmail.com>
2016-05-16 09:09:37 +0800

    代理设置成功

--- proxy.config.js
+++ proxy.config.js
@@ -1,3 +1,6 @@
 
-module.exports = {};
+module.exports = {
+    'http://192.168.20.206:8989/elink_scm_web/*': 'http://192.168.20.206:8080',
+    'http://192.168.20.206:8080/elink_scm_web/*': 'http://192.168.20.206:8080',
+};
 
