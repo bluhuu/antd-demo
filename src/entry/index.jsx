549b29a8719be7520a2f6d5a3878b4d449082be8
bluhuu <bluhuu@gmail.com>
2016-06-13 14:28:08 +0800

    开始做 menu 加 tab

--- src/entry/index.jsx
+++ src/entry/index.jsx
@@ -3,7 +3,7 @@ import App from '../component/App_pag';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import Header from '../component/Header';
-import NavbarSide from '../component/AsideTree2';
+import NavbarSide from '../component/NavbarSide';
 import MyModalForm from '../component/MyModalForm';
 import CTabs from '../component/CTabs';
 
@@ -13,7 +13,7 @@ import { Menu, Icon } from 'antd';
 import AsideTree from '../component/AsideTree';
 ReactDOM.render(
     <div className="ant-layout-container">
-      <CTabs></CTabs>
+      <CTabs/>
 {/*      <div className="ant-layout-main" style={{float:'right',width:'100%'}}>
         <div className="ant-layout-container">
           <div className="ant-layout-content">
