5c1678d457760dae4cc011dab354552d4299c89d
bluhuu <bluhuu@gmail.com>
2016-06-13 15:07:01 +0800

    step.1

---
+++ src/component/Contents.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import CTabs from '../component/CTabs';
+import NavbarSide from '../component/NavbarSide';
+const Contents = React.createClass({
+  render() {
+    return (
+        <div>
+          <nav className="navbar-default navbar-side" role="navigation">
+            <NavbarSide />
+          </nav>
+          <div id="page-wrapper" className="border table-bordered">
+              <div id="page-inner">
+                <div className="row">
+                  <div className="col-md-12 col-sm-12 col-xs-12">
+                    <div className="board">
+                      <div className="panel panel-primary">
+                        <CTabs />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <footer><p>Copyright 2016版权所有@<a href='http://www.nyysfw.com' target='_blank'>南京医药药事服务有限公司</a> </p>
+                </footer>
+              </div>
+          </div>
+        </div>
+    );}
+});
+export default Contents;
+
--- src/entry/index.jsx
+++ src/entry/index.jsx
@@ -1,35 +1,9 @@
 import '../common/lib';
-import App from '../component/App_pag';
-import ReactDOM from 'react-dom';
 import React from 'react';
-import Header from '../component/Header';
-import NavbarSide from '../component/NavbarSide';
-import MyModalForm from '../component/MyModalForm';
-import CTabs from '../component/CTabs';
+import ReactDOM from 'react-dom';
+import Contents from '../component/Contents';
 
-//import BrowserDemo from '../component/BrowserDemo';
-// import Breadcrumb from '../components/breadcrumb/index';
-import { Menu, Icon } from 'antd';
-import AsideTree from '../component/AsideTree';
 ReactDOM.render(
-    <div className="ant-layout-container">
-      <CTabs/>
-{/*      <div className="ant-layout-main" style={{float:'right',width:'100%'}}>
-        <div className="ant-layout-container">
-          <div className="ant-layout-content">
-            <div style={{ height: 590 }}>
-              <MyModalForm />
-              <App url="/elink_scm_web/sproductAction/query.do"/>
-            </div>
-          </div>
-        </div>
-      </div>*/}
-    </div>
+    <Contents />
     , document.getElementById('react-content'));
-
-ReactDOM.render(
-    <div className="ant-layout-side">
-      <NavbarSide />
-    </div>
-    , document.getElementById('react-side'));
 
