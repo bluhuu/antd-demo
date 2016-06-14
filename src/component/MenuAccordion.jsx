814440fdf259933fa24aa9a844696adda837b145
bluhuu <bluhuu@gmail.com>
2016-06-14 18:02:42 +0800

    Signed-off-by: bluhuu <bluhuu@gmail.com>

--- src/component/MenuAccordion.jsx
+++ src/component/MenuAccordion.jsx
@@ -4,39 +4,38 @@ import { Menu, Icon } from 'antd';
 var SubMenu = Menu.SubMenu;
 
 var MenuAccordion = React.createClass({
-  getInitialState: function() {
-    return {
-      current: '1',
-      openKeys: [],
-      loading: false,
-      data:[]
-    };
-  },
-  fetch: function(params = {}) {
-    var _self = this;
-    this.setState({
-      loading: true
-    });
-    $.ajax({
-        url: this.props.urlA,
-        data: params,
-        dataType: "json",
-        async: false,
-        success: function(result) {
-            var resultList = result.rows || result;
-            _self.setState({
-              loading: false,
-              data: resultList
-            });
-            _self.fetchLeaf(resultList);
-      },
-    });
-  },
-    fetchLeaf(resultData) {
+    getInitialState: function() {
+        return {
+            current: '1',
+            openKeys: [],
+            loading: false,
+            data: []
+        };
+    },
+    fetch: function(params = {}) {
+        var _self = this;
+        this.setState({
+            loading: true
+        });
+        $.ajax({
+            url: this.props.urlA,
+            data: params,
+            dataType: "json",
+            async: false,
+            success: function(result) {
+                var resultList = result.rows || result;
+                _self.setState({
+                    loading: false,
+                    data: resultList
+                });
+                _self.fetchLeaf(resultList);
+            },
+        });
+    },
+    fetchLeaf: function(resultData) {
         var _self = this;
         for (var i = 0, len = resultData.length; i < len; i++) {
             if (!resultData[i].leaf) {
-                // console.log(resultData[i]);
                 $.ajax({
                     url: this.props.urlB,
                     data: {
@@ -48,58 +47,59 @@ var MenuAccordion = React.createClass({
                     success: function(result) {
                         var resultList = result.rows || result;
                         resultData[i].tree = resultList;
-                        // _self.setState({
-                        //     loading: false,
-                        //     data: resultList
-                        // });
                         _self.fetchLeaf(resultData[i].tree);
                     },
                 });
             }
         }
-        //console.log("leaf: ", resultData);
         _self.setState({
             loading: false,
             data: resultData
         });
     },
-  componentDidMount: function() {
-    this.fetch({
-      node:this.props.id,
-      nodeID:this.props.id
-    });
-  },
-  handleClick(e) {
-    this.props.addTab(e);
-    this.setState({
-      current: e.key,
-      openKeys: e.keyPath.slice(1),
-    });
-  },
-  onToggle(info) {
-    this.setState({
-      openKeys: info.open ? info.keyPath : info.keyPath.slice(1),
-    });
-  },
-  render: function() {
-    var repos = this.state.data;
-    console.log("render: ", repos);//完整树结构
-    var repoList = repos.map(function (repo) {
-      if(repo.leaf){
-        return(
-          <Menu.Item key={repo.id}>{repo.text}</Menu.Item>
-          )
-      }else{
-        return (
-          <SubMenu key={repo.id} title={<span><Icon type="appstore" /><span>{repo.text}</span></span>}>
-            {/*<MenuAccordionA id={repo.id} url="/elink_scm_web/menuTreeAction/tree.do" />*/}
-          </SubMenu>
-        );
-      }
-    });
+    componentDidMount: function() {
+        this.fetch({
+            node: this.props.id,
+            nodeID: this.props.id
+        });
+    },
+    handleClick(e) {
+        this.props.addTab(e);
+        this.setState({
+            current: e.key,
+            openKeys: e.keyPath.slice(1),
+        });
+    },
+    onToggle(info) {
+        this.setState({
+            openKeys: info.open ? info.keyPath : info.keyPath.slice(1),
+        });
+    },
+    render: function() {
+        var _self = this;
+        var repos = this.state.data;
+        console.log("render: ", repos); //完整树结构s
 
-    return (
-      <Menu onClick={this.handleClick}
+        // var repoList = this.renderList(repos);
+        var repoList = repos.map(function(repo) {
+            if (repo.leaf) {
+                return (
+                    <Menu.Item key={repo.id}>{repo.text}</Menu.Item>
+                )
+            } else {
+                return (
+                    <SubMenu key={repo.id} title={<span><Icon type="appstore" /><span>{repo.text}</span></span>}>
+                        {/*<MenuAccordionA id={repo.id} url="/elink_scm_web/menuTreeAction/tree.do" />*/}
+                            {/*_self.renderList(repo.tree);*/}
+                        </SubMenu>
+                );
+            }
+        });
+
+        console.log("repoList: ", repoList);
+
+        return (
+            <Menu onClick={this.handleClick}
         style={{ width: 240 }}
         openKeys={this.state.openKeys}
         onOpen={this.onToggle}
@@ -110,7 +110,7 @@ var MenuAccordion = React.createClass({
       >
         {repoList}
       </Menu>
-    );
-  },
+        );
+    },
 });
 export default MenuAccordion;
